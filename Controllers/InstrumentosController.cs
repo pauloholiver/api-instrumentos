c456ddad704b47efbb9de73fad803a03ec2c6491
Paulo Henrique Lopes <55982445+pauloholiver@users.noreply.github.com>
2021-07-01 08:58:45 -0300

    Update final API

---
+++ Controllers/InstrumentosController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiInstrumentos.Data;
+
+namespace ApiInstrumentos.Controllers
+{
+    [Route("api/instrumentos")]
+    [ApiController]
+    public class InstrumentosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public InstrumentosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // ex -> GET: api/Instrumentos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Instrumento>>> GetInstrumentos()
+        {
+            return await _context.Instrumentos.ToListAsync();
+        }
+
+        // GET: api/Instrumentos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Instrumento>> GetInstrumento(int id)
+        {
+            var instrumento = await _context.Instrumentos.FindAsync(id);
+
+            if (instrumento == null)
+            {
+                return NotFound();
+            }
+
+            return instrumento;
+        }
+
+        // ex -> PUT: api/Instrumentos/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutInstrumento(int id, Instrumento instrumento)
+        {
+            if (id != instrumento.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(instrumento).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InstrumentoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // ex -> POST: api/Instrumentos
+        [HttpPost]
+        public async Task<ActionResult<Instrumento>> PostInstrumento(Instrumento instrumento)
+        {
+            _context.Instrumentos.Add(instrumento);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetInstrumento", new { id = instrumento.id }, instrumento);
+        }
+
+        // ex -> DELETE: api/Instrumentos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteInstrumento(int id)
+        {
+            var instrumento = await _context.Instrumentos.FindAsync(id);
+            if (instrumento == null)
+            {
+                return NotFound();
+            }
+
+            _context.Instrumentos.Remove(instrumento);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool InstrumentoExists(int id)
+        {
+            return _context.Instrumentos.Any(e => e.id == id);
+        }
+    }
+}
---
+++ Data/Instrumento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiInstrumentos.Data
+{
+    public class Instrumento
+    {
+        public int id { get; set; }
+        public string descricao { get; set; }
+        public decimal preco { get; set; }
+        public string imagem { get; set; }
+
+    }
+}
